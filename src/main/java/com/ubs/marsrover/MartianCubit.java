28da2f14f1fd66e2f20c43642d7847c157e66719
smalaca <ab13.krakow@gmail.com>
2023-03-31 13:40:51 +0200

    GREEN - should land on zero-zero

--- src/main/java/com/ubs/marsrover/MartianCubit.java
+++ src/main/java/com/ubs/marsrover/MartianCubit.java
@@ -1,5 +1,8 @@
 package com.ubs.marsrover;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class MartianCubit {
     private final int xAxis;
     private final int yAxis;
@@ -7,5 +10,21 @@ public class MartianCubit {
     MartianCubit(int xAxis, int yAxis) {
         this.xAxis = xAxis;
         this.yAxis = yAxis;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MartianCubit that = (MartianCubit) o;
+
+        return new EqualsBuilder().append(xAxis, that.xAxis).append(yAxis, that.yAxis).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(xAxis).append(yAxis).toHashCode();
     }
 }
